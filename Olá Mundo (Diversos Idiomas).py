ef4a73a58bb32479745fd2c3bf7c3da200b03a95
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2021-11-22 14:33:44 -0300

    Add files via upload

---
+++ Olá Mundo (Diversos Idiomas).py
@@ -0,0 +1,98 @@
+print('Digite 1 para exibir a mensagem na língua portuguesa.')
+print('Digite 2 para exibir a mensagem na língua espanhola.')
+print('Digite 3 para exibir a mensagem na língua francesa.')
+print('Digite 4 para exibir a mensagem na língua italiana.')
+print('Digite 5 para exibir a mensagem na língua inglesa.')
+print('Digite 6 para exibir a mensagem na língua alemã.')
+print('Digite 7 para exibir a mensagem na língua dinamarquesa.')
+print('Digite 8 para exibir a mensagem na língua holandesa.')
+print('Digite 9 para exibir a mensagem na língua norueguesa.')
+print('Digite 10 para exibir a mensagem na língua sueca.')
+print('Digite 11 para exibir a mensagem na língua finlandesa.')
+print('Digite 12 para exibir a mensagem na língua islandesa.')
+print('Digite 13 para exibir a mensagem na língua grega.')
+print('Digite 14 para exibir a mensagem na língua latina.')
+print('Digite 15 para exibir a mensagem na língua romena.')
+print('Digite 16 para exibir a mensagem na língua polonesa.')
+print('Digite 17 para exibir a mensagem na língua eslovaca.')
+print('Digite 18 para exibir a mensagem na língua sérvia.')
+print('Digite 19 para exibir a mensagem na língua eslovena.')
+print('Digite 20 para exibir a mensagem na língua croata.')
+print('Digite 21 para exibir a mensagem na língua húngara.')
+print('Digite 22 para exibir a mensagem na língua turca.')
+print('Digite 23 para exibir a mensagem na língua ucraniana.')
+print('Digite 24 para exibir a mensagem na língua russa.')
+print('Digite 25 para exibir a mensagem na língua hebraica.')
+print('Digite 26 para exibir a mensagem na língua árabe.')
+print('Digite 27 para exibir a mensagem na língua chinesa.')
+print('Digite 28 para exibir a mensagem na língua coreana.')
+print('Digite 29 para exibir a mensagem na língua japonesa.')
+print('Digite 30 para exibir a mensagem na língua hindi.')
+
+print('')
+print('')
+
+op = int(input())
+if(op==1):
+    print('Olá Mundo.')
+elif(op==2):
+    print('Hola mundo.')
+elif(op==3):
+    print('Bonjour le monde.')
+elif(op==4):
+    print('Ciao mondo.')
+elif(op==5):
+    print('Hello World.')
+elif(op==6):
+    print('Hallo Welt.')
+elif(op==7):
+    print('Hej Verden.')
+elif(op==8):
+    print('Hallo Wereld.')
+elif(op==9):
+    print('Hei Verden.')
+elif(op==10):
+    print('Hej världen.')
+elif(op==11):
+    print('Hei maailma.')
+elif(op==12):
+    print('Halló heimur.')
+elif(op==13):
+    print('Γειά σου Κόσμε (Geiá sou Kósme).')
+elif(op==14):
+    print('Salve Mundo.')
+elif(op==15):
+    print('Salut Lume.')
+elif(op==16):
+    print('Witaj świecie.')
+elif(op==17):
+    print('Ahoj svet.')
+elif(op==18):
+    print('Здраво Свете (Zdravo Svete).')
+elif(op==19):
+    print('Pozdravljen svet.')
+elif(op==20):
+    print('Pozdrav svijete')
+elif(op==21):
+    print('Helló Világ')
+elif(op==22):
+    print('Selam Dünya')
+elif(op==23):
+    print('Привіт Світ (Pryvit Svit)')
+elif(op==24):
+    print('Привет мир (Privet mir)')
+elif(op==25):
+    print('שלום עולם')
+elif(op==26):
+    print('(marhaban bialealam) مرحبا بالعالم')
+elif(op==27):
+    print('(Nǐ hǎo shìjiè) 你好 世界')
+elif(op==28):
+    print('(hellowoldeu) 헬로월드')
+elif(op==29):
+    print('(Konnichiwa sekai) こんにちは世界')
+elif(op==30):
+    print('नमस्ते दुनिया (namaste duniya)')
+
+print('')
+a = input()
